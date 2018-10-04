fd5dbb871935e40be3a547eed2117598b3127332
TylerH123 <thuang00@stuy.edu>
2018-10-04 11:45:17 -0400

    BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -19,27 +19,33 @@ public class BankAccount{
 	public void setPass(String s){
 		password = s;
 	}
-	public void deposit(int n){
+	public boolean deposit(double n){
 		if (n > 0){
 			balance += n;
-			System.out.println("Deposit successful. Current balance: " + balance);
+			return true;
 		}
 		else{
-			System.out.println("Deposit failed. Current balance: " + balance);
+			return false;
 		}
 	}
-	public void withdraw(int w){
+	public boolean withdraw(double w){
 		if (w >= 0){
 			if (balance >= w){
 				balance -= w;
-				System.out.println("Withdrawl successful. Current balance: " + balance);
+				return true;
 			}
 			else{
-				System.out.println("Withdrawl failed. Not enough funds. Current balance: " + balance);
+				return false;
 			}
 		}
 		else{
-			System.out.println("Withdrawl failed. Current balance: " + balance);
+			return false;
 		}
+	}
+	private boolean authenticate(String pass){
+		return password.equals(pass);
+	}
+	public boolean transferTo(BankAccount other, double amount, String password){
+		
 	}
 }
